a0d024f07f1e401a9a147952337209bacb0c9312
Mahsa Nurfarhan Hidayat <farhan@budiluhur.sch.id>
2026-04-01 13:02:07 +0700

    Enable password grant and configure token expiration for authentication API

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Passport::enablePasswordGrant();
+
+        Passport::tokensExpireIn(now()->addMinutes(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+
         RateLimiter::for('api-login', function (Request $request) {
             return Limit::perMinute(5)->by($request->ip());
         });
