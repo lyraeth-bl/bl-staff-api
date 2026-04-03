67eb3c5b930eca7b69703f0ef7ab679ea9b399eb
Mahsa Nurfarhan Hidayat <farhan@budiluhur.sch.id>
2026-04-03 11:02:18 +0700

    Implement login and logout functionality in AuthController; add LoginRequest validation and update Postman requests

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,12 @@ Route::prefix('v1')->group(function () {
     // Auth routes (public)
     Route::prefix('auth')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
     });
 
-    Route::middleware('auth:sanctum')->group(function () {});
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('auth')->group(function () {
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
+    });
 });
